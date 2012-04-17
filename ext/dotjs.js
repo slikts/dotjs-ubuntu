db5a252de326e7247eef4395d607d759d1646829
Reinis Ivanovs <reinis.ivanovs@accenture.com>
2012-04-17 18:25:06 +0300

    Make it also serve .css files if the .js file exists

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,21 @@
-$.ajax({
-  url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
-})
+(function($) {
+    var url_base = 'http://localhost:3131/' + window.location.host.replace(/^www\./, '');
+    
+    $.ajax({
+        url: url_base + '.js',
+        dataType: 'text',
+        success: function(data) {
+            $(function() {
+                eval(data);
+            });
+            $('<link>').attr({
+                rel: 'stylesheet',
+                type: 'text/css',
+                href: url_base + '.css'
+            }).appendTo('head');
+        },
+        error: function(){
+            console.log('no dotjs server found at localhost:3131');
+        }
+    });
+})(jQuery);
